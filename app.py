df67f22a02cce69a52f782c147d497f24feaf478
Arthur <138954266+sensewxrld@users.noreply.github.com>
2024-04-04 22:29:03 -0300

    aula_1

--- app.py
+++ app.py
@@ -1,41 +1,19 @@
-import os
-
-def exibir_nome_do_programa():
-    print("""
+print( """
 ░██████╗░█████╗░██████╗░░█████╗░██████╗░  ███████╗██╗░░██╗██████╗░██████╗░███████╗░██████╗░██████╗
 ██╔════╝██╔══██╗██╔══██╗██╔══██╗██╔══██╗  ██╔════╝╚██╗██╔╝██╔══██╗██╔══██╗██╔════╝██╔════╝██╔════╝
 ╚█████╗░███████║██████╦╝██║░░██║██████╔╝  █████╗░░░╚███╔╝░██████╔╝██████╔╝█████╗░░╚█████╗░╚█████╗░
 ░╚═══██╗██╔══██║██╔══██╗██║░░██║██╔══██╗  ██╔══╝░░░██╔██╗░██╔═══╝░██╔══██╗██╔══╝░░░╚═══██╗░╚═══██╗
 ██████╔╝██║░░██║██████╦╝╚█████╔╝██║░░██║  ███████╗██╔╝╚██╗██║░░░░░██║░░██║███████╗██████╔╝██████╔╝
 ╚═════╝░╚═╝░░╚═╝╚═════╝░░╚════╝░╚═╝░░╚═╝  ╚══════╝╚═╝░░╚═╝╚═╝░░░░░╚═╝░░╚═╝╚══════╝╚═════╝░╚═════╝░
-    """)
+      """)
 
-def exibir_opcoes():
-    print('1. Cadastrar restaurante')
-    print('2. Listar restaurantes')
-    print('3. Ativar restaurante')
-    print('4. Sair\n')
+print ('1. Cadastrar restaurante')
+print ('2. Listar restaurante')
+print ('3. Ativar restaurante')
+print ('4. Sair\n')
 
-def finalizar_app():
-    os.system('cls')
-    print('Finalizando o app')
-
-def escolher_opcao():
-    opcao_escolhida = int(input('Escolha uma opção: '))
-
-    if opcao_escolhida == 1:
-        print('Cadastrar restaurante')
-    elif opcao_escolhida == 2:
-        print('Listar restaurantes')
-    elif opcao_escolhida == 3:
-        print('Ativar restaurante')
-    else:
-        finalizar_app()
-
-def main():
-    exibir_nome_do_programa()
-    exibir_opcoes()
-    escolher_opcao()
+opcao_escolhida = input('Escolha uma opção: ')
+print (f'Você escolheu a opção {opcao_escolhida}')
 
 if __name__ == '__main__':
     main()
